0681dcd5a539ca4c9ee4e284a0453b02a526cbd0
shubham <shubhamtorvi18@gmail.com>
2018-09-30 00:44:04 +0530

    nightshift

---
+++ NHUB/DAL/Repository/AddNotificationRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace DAL.Repository
+{
+    public class AddNotificationRepository
+    {
+
+        public int InsertEvent(string Name, int Sourceid)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = Connections.Constring;
+            connection.Open();
+            // Note the "placeholders" in the SQL query.
+            string sql = "Insert Into Event" +
+                                "(Name, SourceId) Values" +
+                                "(@Name, @Sourceid)";
+
+
+            // This command will have internal parameters.
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                // Fill params collection.
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@Name",
+                    Value = Name,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 30
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Sourceid",
+                    Value = Sourceid,
+                    SqlDbType = SqlDbType.Int,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                //parameter = new SqlParameter
+                //{
+                //    ParameterName = "@Mandatory",
+                //    Value = Mandetory,
+                //    SqlDbType = SqlDbType.Bit,
+                //    Size = 1
+                //};
+                //command.Parameters.Add(parameter);
+
+                //parameter = new SqlParameter
+                //{
+                //    ParameterName = "@Confidential",
+                //    Value = Confidential,
+                //    SqlDbType = SqlDbType.Bit,
+                //    Size = 1
+                //};
+                //command.Parameters.Add(parameter);
+
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Cant insert Data!", ex);
+                    throw error;
+                }
+            }
+            //string str = "select SCOPE_IDENTITY()";
+            //SqlCommand sqlCommand = new SqlCommand(str, connection);
+            //object o = sqlCommand.ExecuteScalar();
+            //long y= Convert.ToInt64(o);
+            string str = "select max(Id) from event";
+            SqlCommand sqlCommand = new SqlCommand(str, connection);
+            int y = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            connection.Close();
+             return y;
+        }
+        public void InsertEventChannel(long id, int channelid)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = Connections.Constring;
+            connection.Open();
+            // Note the "placeholders" in the SQL query.
+            string sql = "Insert Into EventChannel" +
+                                "(EventId, ChannelId) Values" +
+                                "(@Eventid, @Channelid)";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                // Fill params collection.
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@Eventid",
+                    Value = id,
+                    SqlDbType = SqlDbType.Int,
+                    Size = 30
+                };
+                command.Parameters.Add(parameter);
+                 parameter = new SqlParameter
+                {
+                    ParameterName = "@Channelid",
+                    Value = channelid,
+                    SqlDbType = SqlDbType.Int,
+                    Size = 30
+                };
+                command.Parameters.Add(parameter);
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Cant insert Data!", ex);
+                    throw error;
+                }
+            }
+        }
+       
+    }
+    
+}
--- NHUB/NHUB/Notifications.aspx.cs
+++ NHUB/NHUB/Notifications.aspx.cs
@@ -4,47 +4,68 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DAL.Repository;
 
 namespace NHUB
 {
     public partial class Notifications : System.Web.UI.Page
     {
+        NotificationsRepository notificationsRepository = new NotificationsRepository();
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            if (!(Page.IsPostBack))
+            
+            notificationsRepository.getDetails();
+            if (!Page.IsPostBack)
             {
-                for (int count = 0; count < 5; count++)
-                {
-                    Label NotificationName = new Label();
-                    HyperLink EditLink = new HyperLink();
-                    HyperLink DeleteLink = new HyperLink();
-                    NotificationName.Text = "Shubham" + 1;
-                    NotificationName.Width = 200;
-                    NotificationPlaceHolder.Controls.Add(NotificationName);
-                    EditLink.Text = "Edit";
-                    EditLink.Width = 200;
-                    NotificationPlaceHolder.Controls.Add(EditLink);
-                    DeleteLink.Text = "Delete" + "<br/>";
+                HyperLink NameHyperlink;
 
-                    NotificationPlaceHolder.Controls.Add(DeleteLink);
+                for (int Sourcecount = 0; Sourcecount < notificationsRepository.SourceList.Count; Sourcecount++)
+                {
+                    NameHyperlink = new HyperLink();
+                    NameHyperlink.Text = notificationsRepository.SourceList[Sourcecount].SourceName+"<br/>";
+                    NameHyperlink.ID = "count";
+                    NotificationPlaceHolder.Controls.Add(NameHyperlink);
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Label lb = new Label();
+                        lb.Text = "shubham" + i + "<br/>";
+                        lb.Visible = true;
+                        if (lb.Visible == false)
+                        {
+
+                        }
+                        else
+                        {
+
+                            NotificationPlaceHolder.Controls.Add(lb);
+                        }
+                    } 
+                    
                 }
             }
+
+            //else
+            //{
+            //    int id = Convert.ToInt32(Request.QueryString["id"]);
+            //    Label lb;
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        lb = new Label();
+            //        lb.Text = "shubham" + id + i + "<br/>";
+            //        NotificationPlaceHolder.Controls.Add(lb);
+
+            //    }
+            //}
+
         }
+       
+        
 
-        protected void TestButton_Click(object sender, EventArgs e)
+
+        protected void AddNotificationButton_Click(object sender, EventArgs e)
         {
-
-        }
-
-        protected void Test2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
+            Response.Redirect("AddEvent.aspx");
         }
     }
 }
